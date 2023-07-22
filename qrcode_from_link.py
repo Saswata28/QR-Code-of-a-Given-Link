3a550330727be253bd6912c0eb2819fe577d51bf
Saswata28 <94910016+Saswata28@users.noreply.github.com>
2023-07-23 00:18:00 +0530

    Update qrcode_from_link.py

--- qrcode_from_link.py
+++ qrcode_from_link.py
@@ -1,11 +1,8 @@
-# pip install qrcode Image
+# Do 'pip install qrcode Image' in your terminal first.
 import qrcode
-import os
-import random
 
 
 def generate_qr(link, file_name):
-    # This will generate the QR code from the link.
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -13,48 +10,10 @@ def generate_qr(link, file_name):
         border=4,
     )
 
-    # Adding the link inputted by user in the QR.
     qr.add_data(link)
     qr.make(fit=True)
     image_of_qr = qr.make_image(fill_color="black", back_color="white")
     image_of_qr.save(f"{file_name}.png")
-
-    # finding whether same name file exists in the diectory or not.
-    # user_input = input('What is the name of your directory')
-    # current_directory = os.getcwd()
-    # directory = current_directory.split("\\")[-1]
-    # print(directory)
-    # while_loop_continue = False
-    # while_loop_break = False
-    # while True:
-    #     random_number = random.randint(0, 1000)
-
-    #     searchstring = f"QR_Code{random_number}.png"
-    #     # print(searchstring)
-
-    #     for fname in directory:
-    #         if os.path.isfile(directory + os.sep + fname):
-    #             print("yes")
-    #             # We use user_input + os.sep + fname to get full path.os.listdir gives files and directories names, so we use os.path.isfile to check for files.
-    #             # Full path
-    #             # if os.path.isfile(directory):
-    #             f = open(directory + os.sep + fname, 'r')
-    #             # f = open(directory, 'r')
-
-    #             if searchstring in f.read():
-    #                 print('found string in file %s' % fname)
-    #                 while_loop_continue = True
-    #             else:
-    #                 print('string not found')
-    #                 image_of_qr.save(f"QR_Code{random_number}.png")
-    #                 while_loop_break = True
-    #             f.close()
-    #     if while_loop_break == True:
-    #         break
-    #     elif while_loop_continue == True:
-    #         continue
-    # image_of_qr.save(f"{link}.png")
-    # save the image as a PNG file with the use of Image package.Save the image in the same folder in which this code is.
 
 
 user_input_link = input("Enter the Link you want to get the QR code of: ")
